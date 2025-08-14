9dc9a44789a649196fc4abce65c426a01082ed68
Zhang Yanpo <drdr.xp@gmail.com>
2025-08-14 11:50:33 +0800

    M  _building/__init__.py

--- _building/__init__.py
+++ _building/__init__.py
@@ -13,10 +13,10 @@ import sys
 # __url__ = 'https://requests.readthedocs.io'
 # __version__ = '2.23.0'
 
-__author__ = 'Zhang Yanpo'
-__author_email__ = 'drdr.xp@gmail.com'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2020 Zhang Yanpo'
+__author__ = "Zhang Yanpo"
+__author_email__ = "drdr.xp@gmail.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2020 Zhang Yanpo"
 
 
 # Configuration file for the Sphinx documentation builder.
@@ -31,14 +31,14 @@ __copyright__ = 'Copyright 2020 Zhang Yanpo'
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    'sphinx.ext.napoleon',
+    "sphinx.ext.napoleon",
     # "sphinx.ext.intersphinx",
     # "sphinx.ext.todo",
     # "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -54,7 +54,7 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -63,33 +63,28 @@ html_theme = 'alabaster'
 html_static_path = []
 
 
-def sphinx_confs():
+def load_parent_package():
     """
-        Load repo dir as a package
+    Load the parent directory as a package module.
 
-        `readthedocs` use branch name as dir!
-        Thus the following does not work::
-
-            import pk3proc
+    Returns:
+        tuple: (package_name, package_module)
     """
-
-    print("sys.path:", sys.path)
-
-    # Add the parent directory to sys.path temporarily to allow proper imports
     import os
+
     parent_dir = os.path.dirname(os.path.dirname(__file__))
     if parent_dir not in sys.path:
         sys.path.insert(0, parent_dir)
 
     # Read the __init__.py file to get the package name
-    init_file = os.path.join(parent_dir, '__init__.py')
+    init_file = os.path.join(parent_dir, "__init__.py")
     package_name = None
 
-    with open(init_file, 'r') as f:
+    with open(init_file, "r") as f:
         for line in f:
-            if line.strip().startswith('__name__'):
+            if line.strip().startswith("__name__"):
                 # Extract package name from __name__ = "package_name"
-                package_name = line.split('=')[1].strip().strip('"\'')
+                package_name = line.split("=")[1].strip().strip("\"'")
                 break
 
     if not package_name:
@@ -101,6 +96,23 @@ def sphinx_confs():
     pkg = importlib.util.module_from_spec(spec)
     sys.modules[package_name] = pkg  # Add to sys.modules so relative imports work
     spec.loader.exec_module(pkg)
+
+    return package_name, pkg
+
+
+def sphinx_confs():
+    """
+    Load repo dir as a package
+
+    `readthedocs` use branch name as dir!
+    Thus the following does not work::
+
+        import pk3proc
+    """
+
+    print("sys.path:", sys.path)
+
+    package_name, pkg = load_parent_package()
 
     return (
         pkg.__name__,
